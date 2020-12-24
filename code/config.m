b32f20bac562bfe14f39764aa0eec4d66e4cf426
giuliopcfc <56389329+giuliopcfc@users.noreply.github.com>
2020-12-24 01:06:08 +0100

    update

--- code/config.m
+++ code/config.m
@@ -1,59 +1,87 @@
 data = struct();
 
-% Inertia properties of the spacecraft:
-data.sc.mass = 1225;
-data.sc.Ix = 700;
-data.sc.Iy = 1100;
-data.sc.Iz = 1300;
+%% Inertia properties of the spacecraft:
+data.sc.mass = 12;
+data.sc.Ix = 182500e-6;
+data.sc.Iy = 187200e-6;
+data.sc.Iz = 110500e-6;
 data.sc.inertiaMatrix = [data.sc.Ix 0 0; 0 data.sc.Iy 0; 0 0 data.sc.Iz];
 
-% Magnetic Torque:
+%% Magnetic Torque:
 data.magneticTorque.m = [0.1; 0.1; 0.1];
 
-% SRP:
-alpha = 67.5*pi/180;
+%% SRP:
+
+% Normal versors:
 n1 = [1; 0; 0]; n2 = [0; 1; 0]; n3 = [0; 0; 1];
-n7 = [0; -cos(alpha); -sin(alpha)];
-data.SRP.NBMat = [n1, n2, n3, -n1, -n2, -n3, n7, -n7];
+NBMat = nan(3,14);
+NBMat(:,1) = n1; NBMat(:,2) = -n1;
+NBMat(:,3) = n2; NBMat(:,4) = -n2;
+NBMat(:,[5 7 9 11 13]) = n3.*ones(3,5); NBMat(:,[6 8 10 12 14]) = -n3.*ones(3,5);
+data.SRP.NBMat = NBMat;
 
-lxB = 3.4; lyB = 1.8; lzB = 2.35; 
-wP = 3.9; hP = 2.2; aPanel = wP*hP;
-aB14 = lyB*lzB; aB25 = lxB*lzB; aB36 = lxB*lyB;
-data.SRP.aSurf = [aB14;aB25;aB36;aB14;aB25;aB36;aPanel*[1;1]];
+% Areas:
+U = 100e-3;
+a1 = 4*U^2; a2 = 2*3*U^2; 
+data.SRP.aSurf = [a2*ones(4,1); a1*[1;1]; a2*ones(8,1)];
 
-rCOM = [1.331; -0.066; 0.035];
-r1 = [lxB;0;0]; r4 = [0;0;0];
-r2 = [0; lyB/2; 0]; r5 = -r2;
-r3 = [0; 0; lzB/2]; r6 = -r3;
-r7 = [hP/2;
-      -lyB/2 - wP/2*sin(alpha);
-      -lzB/2 + wP/2*cos(alpha)];
-r8 = r7;
-      
-data.SRP.rSurf = [r1, r2, r3, r4, r5, r6, r7, r7, r8]-rCOM;
+% Positions:
+rCOM = [-25e-3; 37e-3; 130e-3];
+
+rSurf1 = nan(3,14);
+rSurf1(:,1) = [1*U; 0; 1.5*U]; rSurf1(:,2) = [-1*U; 0; 1.5*U];
+rSurf1(:,3) = [0; 1*U; 1.5*U]; rSurf1(:,4) = [0; -1*U; 1.5*U];
+rSurf1(:,5) = [0; 0; 3*U]; rSurf1(:,6) = [0; 0; 0];
+rSurf1(:,[7 8]) = [2.5*U; 0; 0].*ones(3,2); rSurf1(:,[9 10]) = [-2.5*U; 0; 0].*ones(3,2);
+rSurf1(:,[11 12]) = [0; 2.5*U; 0].*ones(3,2); rSurf1(:,[13 14]) = [0; -2.5*U; 0].*ones(3,2);
+
+data.SRP.rSurf = rSurf1-rCOM;
 
 rhoSB = 0.5; rhoSP = 0.5; rhoDB = 0.1; rhoDP = 0.1;
-data.SRP.rhoS = [rhoSB*ones(6,1); rhoSP*ones(2,1)];
-data.SRP.rhoD = [rhoDB*ones(6,1); rhoDP*ones(4,1)];
+data.SRP.rhoS = [rhoSB*ones(6,1); rhoSP*ones(8,1)];
+data.SRP.rhoD = [rhoDB*ones(6,1); rhoDP*ones(8,1)];
 
+%% Star Sensor:
+% load HYG
+% data.starSensor.versStars = HYG.VERS(1:100:end,:);
+% data.starSensor.FOV = 14*pi/180;
+% data.starSensor.variance = (1/3600*pi/180)^2;
+% data.starSensor.sampleTime = 1/5;
 
-% Orbit:
-data.orbit.a = 7160;
-data.orbit.e = 1.4e-4;
-data.orbit.i = 98.56*pi/180;
+%% Gyroscope:
+% data.gyroscope.sampleTime = 1/262;
+% data.gyroscope.sigmaN = 2.6957e-06;
+% data.gyroscope.sigmaB = 1.4976e-12;
+
+%% Reaction Wheels:
+data.reactionWheel.axis = eye(3);
+data.reactionWheel.hMax = 30e-3;
+data.reactionWheel.MMax = 2.3e-3;
+data.reactionWheel.h0 = [0; 0; 0];
+
+%% Magnetorquer:
+data.magnetorquer.m0 = 1.19; 
+
+%% Detumbling:
+data.detumbling.times = [0 800 5000];
+data.detumbling.kProp = -1e-1;
+%% Orbit:
+data.orbit.a = 7271;
+data.orbit.e = 0.02;
+data.orbit.i = 70*pi/180;
 data.orbit.OM = 0*pi/180;
 data.orbit.om = 0*pi/180;
 
-% Initial conditions:
-data.ic.w = [5;10;-7]*pi/180;
+%% Initial conditions:
+data.ic.w = [8;10;-7]*pi/180*1e-4;
 data.ic.dcm = eye(3);
 data.ic.quaternions = [0;0;0;1];
 data.ic.th = 0;
 
-% Constants:
+%% Constants:
 data.const.MU = 398600;
 
-% Additional Parameters:
+%% Additional Parameters:
 
 % Rotation matrix perifocal to ECI:
 OM = data.orbit.OM; i = data.orbit.i; om = data.orbit.om;
@@ -63,3 +91,6 @@ Rom = [ cos(om) sin(om) 0; -sin(om) cos(om) 0; 0 0 1];
 data.orbit.per2ECI = (Rom*Ri*ROM)';
 
 data.orbit.period = 2*pi*sqrt(data.orbit.a^3/data.const.MU);
+
+%% Clear Vars:
+clearvars -except data
---
+++ code/tests/model-1/config.m
@@ -0,0 +1,79 @@
+data = struct();
+
+% Inertia properties of the spacecraft:
+data.sc.mass = 1225;
+data.sc.Ix = 700;
+data.sc.Iy = 1100;
+data.sc.Iz = 1300;
+data.sc.inertiaMatrix = [data.sc.Ix 0 0; 0 data.sc.Iy 0; 0 0 data.sc.Iz];
+
+% Magnetic Torque:
+data.magneticTorque.m = [0.1; 0.1; 0.1];
+
+% SRP:
+alpha = 67.5*pi/180;
+n1 = [1; 0; 0]; n2 = [0; 1; 0]; n3 = [0; 0; 1];
+n7 = [0; -cos(alpha); -sin(alpha)];
+data.SRP.NBMat = [n1, n2, n3, -n1, -n2, -n3, n7, -n7];
+
+lxB = 3.4; lyB = 1.8; lzB = 2.35; 
+wP = 3.9; hP = 2.2; aPanel = wP*hP;
+aB14 = lyB*lzB; aB25 = lxB*lzB; aB36 = lxB*lyB;
+data.SRP.aSurf = [aB14;aB25;aB36;aB14;aB25;aB36;aPanel*[1;1]];
+
+rCOM = [1.331; -0.066; 0.035];
+r1 = [lxB;0;0]; r4 = [0;0;0];
+r2 = [0; lyB/2; 0]; r5 = -r2;
+r3 = [0; 0; lzB/2]; r6 = -r3;
+r7 = [hP/2;
+      -lyB/2 - wP/2*sin(alpha);
+      -lzB/2 + wP/2*cos(alpha)];
+r8 = r7;
+      
+data.SRP.rSurf = [r1, r2, r3, r4, r5, r6, r7, r7, r8]-rCOM;
+
+rhoSB = 0.5; rhoSP = 0.5; rhoDB = 0.1; rhoDP = 0.1;
+data.SRP.rhoS = [rhoSB*ones(6,1); rhoSP*ones(2,1)];
+data.SRP.rhoD = [rhoDB*ones(6,1); rhoDP*ones(4,1)];
+
+% Star Sensor:
+load HYG
+data.starSensor.versStars = HYG.VERS(1:100:end,:);
+data.starSensor.FOV = 14*pi/180;
+data.starSensor.variance = (1/3600*pi/180)^2;
+data.starSensor.sampleTime = 1/5;
+
+% Gyroscope:
+data.gyroscope.sampleTime = 1/262;
+data.gyroscope.sigmaN = 2.6957e-06;
+data.gyroscope.sigmaB = 1.4976e-12;
+
+% Orbit:
+data.orbit.a = 7160;
+data.orbit.e = 1.4e-4;
+data.orbit.i = 98.56*pi/180;
+data.orbit.OM = 0*pi/180;
+data.orbit.om = 0*pi/180;
+
+% Initial conditions:
+data.ic.w = [5;10;-7]*pi/180;
+data.ic.dcm = eye(3);
+data.ic.quaternions = [0;0;0;1];
+data.ic.th = 0;
+
+% Constants:
+data.const.MU = 398600;
+
+% Additional Parameters:
+
+% Rotation matrix perifocal to ECI:
+OM = data.orbit.OM; i = data.orbit.i; om = data.orbit.om;
+ROM = [ cos(OM) sin(OM) 0; -sin(OM) cos(OM) 0; 0 0 1];
+Ri = [ 1 0 0; 0 cos(i) sin(i); 0 -sin(i) cos(i)];
+Rom = [ cos(om) sin(om) 0; -sin(om) cos(om) 0; 0 0 1];
+data.orbit.per2ECI = (Rom*Ri*ROM)';
+
+data.orbit.period = 2*pi*sqrt(data.orbit.a^3/data.const.MU);
+
+%% Clear Vars:
+clearvars -except data
